befc1b6fc1230fbf1fbb9274c72eb871455864c8
Szabolcs Sipos <labuwx@balfug.com>
2016-02-03 22:29:03 +0100

    tmp

---
+++ project_euler/pe26.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def lr(d):
+    nl = []
+    while n not in nl:
+
+
+mv = 0
+for d in range(2, 1000):
+    l = lr(d)
+    if l > mv:
+        mk = d
+        mv = l
+
+print(mk)
+
