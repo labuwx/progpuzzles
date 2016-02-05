a113560b1eb53b798fabb5410adebd89d233e29b
Szabolcs Sipos <labuwx@balfug.com>
2016-02-05 18:19:57 +0100

    PE37

---
+++ project_euler/pe37.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+def isprime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+
+    return n > 1
+
+
+def lp(n):
+    if n > 10:
+        return isprime(n) and lp(int(str(n)[1:]))
+    else:
+        return isprime(n)
+
+
+def rp(n):
+    if n:
+        return isprime(n) and rp(n//10)
+    else:
+        return True
+
+
+k = 0
+s = 0
+for n in range(10, 1000000):
+    if lp(n) and rp(n):
+        k += 1
+        s += n
+        print(str(k) + ': ' + str(n) + ' - ' + str(s))
+
