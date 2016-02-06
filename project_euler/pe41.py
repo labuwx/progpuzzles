cb1e380d6286024360a68f7caf15634fda800258
Szabolcs Sipos <labuwx@balfug.com>
2016-02-07 00:03:56 +0100

    PE41

--- project_euler/pe41.py
+++ project_euler/pe41.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python3
 
 
-def isnpan(k):
-    k = str(k)
-    k = ''.join(sorted(k))
-    #print(k)
+def isprime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return n>1
+
+max = 1
+def check(l):
+    global max
+    n = int(''.join([str(k) for k in l]))
+    if n > max and isprime(n):
+        print(n)
+        max = n
 
 
-for k in range(1, 987654321):
-    isnpan(k)
-    if k%1000000 == 0:
-        print(k)
+def forperm(l, f, t=[]):
+    if l:
+        for i in range(len(l)):
+            p = t[:]
+            p.append(l[i])
+            l2 = l[:]
+            del l2[i]
+            forperm(l2, f, p)
+    else:
+        f(t)
+
+
+for i in range(1, 10):
+    l = list(range(1, i+1))
+    forperm(l, check)
+
+print(max)
 
