a45b756abfc121f075ef9811be6b303d00f3292a
Szabolcs Sipos <(none)>
2017-12-01 14:51:56 +0100

    AoC 2016 day 7

--- advent_of_code/2016/7/7.py
+++ advent_of_code/2016/7/7.py
@@ -7,8 +7,17 @@ input = open('input').read()
 cnt = 0
 for line in input.split('\n'):
     if line == '': continue
-    for blk in re.findall(r'\[?[a-z]*\]?', line):
-        
+    gabba, brackets, aib = False, False, False
+    for i in range(len(line) - 3):
+        if line[i] in '[]':
+            brackets = not brackets
+            continue
+        if set(line[i:i+4]) & set('[]'): continue
+        abba = (line[i:i+2] == line[i+3:i+1:-1]) and (line[i] != line[i+1])
+        aib |= abba and brackets
+        gabba |= abba
+    if gabba and not aib:
+        cnt += 1
 
 
-print(sum)
+print(cnt)
