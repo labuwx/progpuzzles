dd40c6d096a8a3ffd09ab5378fd83061167ae711
Szabolcs Sipos <labuwx@balfug.com>
2017-12-02 14:34:26 +0100

    AoC 2017 day 2

---
+++ advent_of_code/2017/2/2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import itertools as it
+import numpy as np
+
+
+input = np.loadtxt('input', delimiter='\t', dtype=int)
+
+imin = input.min(axis=1)
+imax = input.max(axis=1)
+s1 = (imax - imin).sum()
+
+s2 = 0
+for line in input:
+    for a, b in it.combinations(line, 2):
+        if a%b == 0:
+            s2 += a//b
+            break
+        if b%a == 0:
+            s2 += b//a
+            break
+
+print(s1)
+print(s2)
