ce5ad0cec6d99f4146a5df2e0d50ff9ba421f04f
Szabolcs Sipos <labuwx@balfug.com>
2017-12-04 07:41:40 +0100

    AoC 2017 day 4

---
+++ advent_of_code/2017/4/4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+input = open('input').read()
+
+c1 = c2 = 0
+for passwd in input.split('\n'):
+    if passwd == '': continue
+    parts = passwd.split(' ')
+    parts_s = [str(sorted(word)) for word in parts]
+    if len(parts) == len(set(parts)):
+        c1 += 1
+    if len(parts_s) == len(set(parts_s)):
+        c2 += 1
+
+print(c1)
+print(c2)
