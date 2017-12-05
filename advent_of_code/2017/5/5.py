632e4446be5b95df0fe3adda3852187100aef8a2
Szabolcs Sipos <labuwx@balfug.com>
2017-12-05 08:41:39 +0100

    AoC 2017 day 5

---
+++ advent_of_code/2017/5/5.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+input = open('input').read()
+
+jumps1 = [int(line) for line in input.split('\n') if line!='']
+jumps2 = jumps1.copy()
+
+c1, idx = 0, 0
+while idx in range(len(jumps1)):
+    j = jumps1[idx]
+    jumps1[idx] += 1
+    idx += j
+    c1 += 1
+
+c2, idx = 0, 0
+while idx in range(len(jumps2)):
+    j = jumps2[idx]
+    jumps2[idx] += 1 if j < 3 else -1
+    idx += j
+    c2 += 1
+
+
+print(c1)
+print(c2)
