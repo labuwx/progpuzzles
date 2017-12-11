6071f77cf069bc0bae2b93962490922da2311848
Szabolcs Sipos <labuwx@balfug.com>
2017-12-11 07:36:01 +0100

    AoC 2017 day 11

---
+++ advent_of_code/2017/11/11.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import re
+
+
+smap = {
+    'n': (0, 2),
+    's': (0, -2),
+    'ne': (1, 1),
+    'se': (1, -1),
+    'nw': (-1, 1),
+    'sw': (-1, -1)
+}
+
+def dist(h ,v):
+    h, v = abs(h), abs(v)
+    diags = min(h, v)
+    ndiags = (max(h, v) - diags) / 2
+    return diags + ndiags
+
+
+input = re.sub(r'\s', '', open('input').read()).split(',')
+
+h = v = 0
+dmax = 0
+for s in input:
+    h += smap[s][0]
+    v += smap[s][1]
+    dmax = max(dmax, dist(h, v))
+
+alls = dist(h, v)
+
+print(alls)
+print(dmax)
+
