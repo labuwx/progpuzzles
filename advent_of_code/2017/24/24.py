3722022c703fe6aabb9b7be2913d0e922f4e2ec0
Szabolcs Sipos <labuwx@balfug.com>
2017-12-24 18:44:47 +0100

    AoC 2017 day 24

---
+++ advent_of_code/2017/24/24.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+
+input = open('input').read()
+
+cmps = []
+for line in input.split('\n'):
+    if line == '': continue
+    a, b = [int(w) for w in line.split('/')]
+    cmps.append((a, b))
+
+
+def bb(k, cidx):
+    m = 0
+    for idx in cidx:
+        a, b = cmps[idx]
+        if a == k:
+            pass
+        elif b == k:
+            a, b = b, a
+        else:
+            continue
+        v = bb(b, cidx - {idx})
+        m = max(m, v + a + b)
+    return m
+
+def bb2(k, cidx):
+    m = (0, 0)
+    for idx in cidx:
+        a, b = cmps[idx]
+        if a == k:
+            pass
+        elif b == k:
+            a, b = b, a
+        else:
+            continue
+        l, v  = bb2(b, cidx - {idx})
+        m = max(m, (l+1, v + a + b))
+    return m
+
+
+print(bb(0, set(range(len(cmps)))))
+print(bb2(0, set(range(len(cmps)))))
+
