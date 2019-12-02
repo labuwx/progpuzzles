0ed9edec57c50e91a9f2d992f379f0587c4126ac
Szabolcs Sipos <labuwx@balfug.com>
2019-12-02 18:16:11 +0100

    AoC 2019 day 1

---
+++ advent_of_code/2019/1/1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+def f1(m):
+    return max(m // 3 - 2, 0)
+
+
+def f2(m):
+    s, la = 0, m
+    while(la > 0):
+        la = f1(la)
+        s += la
+    return s
+
+
+input = open('input').read()
+input = [int(x) for x in input.split()]
+
+s1 = sum(f1(m) for m in input)
+s2 = sum(f2(m) for m in input)
+
+print(s1)
+print(s2)
