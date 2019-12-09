32f816b20687ba4fc01794f1a7f2dea53ed009bd
Szabolcs Sipos <labuwx@balfug.com>
2019-12-09 18:54:25 +0100

    AoC 2019 Day 9

---
+++ advent_of_code/2019/9/9.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import operator
+from collections import deque, defaultdict
+
+
+def abs_addr(base, addr, flag):
+    return addr + (0 if flag == 0 else base)
+
+
+def run(mem, inl):
+    mem = defaultdict(int, enumerate(mem))
+    pos, base = 0, 0
+    while True:
+        pm, opc = mem[pos] // 100, mem[pos] % 100
+        if opc == 1:  # add
+            op = operator.add
+            il, ol = 2, 1
+        elif opc == 2:  # mul
+            op = operator.mul
+            il, ol = 2, 1
+        elif opc == 3:  # read
+            op = inl.popleft
+            il, ol = 0, 1
+        elif opc == 4:  # print
+            op = lambda x: x
+            il, ol = 1, 3
+        elif opc == 5:  # jump-if-true
+            op = lambda c, v: v if c else None
+            il, ol = 2, 2
+        elif opc == 6:  # jump-if-false
+            op = lambda c, v: v if not c else None
+            il, ol = 2, 2
+        elif opc == 7:  # less than
+            op = lambda x, y: int(x < y)
+            il, ol = 2, 1
+        elif opc == 8:  # equals
+            op = lambda x, y: int(x == y)
+            il, ol = 2, 1
+        elif opc == 9:  # base adjust
+            op = lambda x: x
+            il, ol = 1, 4
+        elif opc == 99:  # halt
+            break
+        else:
+            print('Wrong opc %d at %d' % (opc, pos))
+            print(list(enumerate(mem)))
+            exit()
+
+        inp = []
+        for _ in range(il):
+            v = mem[(pos := pos + 1)]
+            f, pm = pm % 10, pm // 10
+            inp.append(v if f == 1 else mem[abs_addr(base, v, f)])
+
+        ret = op(*inp)
+        if ol == 1:
+            f, pm = pm % 10, pm // 10
+            mem[abs_addr(base, mem[(pos := pos + 1)], f)] = ret
+        pos += 1
+
+        if ol == 2 and ret != None:
+            pos = ret
+
+        if ol == 4:
+            base += ret
+
+        if ol == 3:
+            print(ret)
+            # yield ret
+
+
+def main():
+    mem = [int(x) for x in open('input').read().strip().split(',')]
+    t0mem = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
+    t1mem = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
+    t2mem = [104, 1125899906842624, 99]
+
+    run(mem, deque([1]))
+    run(mem, deque([2]))
+
+
+main()
