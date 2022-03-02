7988bfe6387d084026e5f48c665f5b27d6f11efc
Szabolcs Sipos <labuwx@balfug.com>
2022-03-02 23:52:08 +0100

    AoC 2021 Day 12: consistency fix + speedup

--- advent_of_code/2021/12/12.py
+++ advent_of_code/2021/12/12.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 
-from collections import Counter, defaultdict
-from heapq import heappush, heappop
+from collections import Counter, defaultdict, deque
 import itertools as it
 
 
 # limitation
 # if there is a loop of big caves that later would turn out to be impossible to finish
-# this algorithm will not terminate
+# this algorithm will not terminate or just give incorrect result
 #
 # idea
 # merge states when possible: when decisions from the past that limit future decisions are the same
 #   = the exact same small caves visited
-# small_cnt is used to visit all states that lead to the same merged state, before visiting the merged state
+# this way there are only combination many visit not permutation many
 
 
-START_NODE, END_NODE = -1, -2
+START_NODE, END_NODE = 0, 1
 
 
 def parse_input(input):
-    neg_cnt, pos_cnt = it.count(-3, -1), it.count()
+    neg_cnt, pos_cnt = it.count(-1, -1), it.count(2, 1)
     node_map = {'start': START_NODE, 'end': END_NODE}
     graph = defaultdict(set)
 
@@ -43,42 +42,54 @@ def parse_input(input):
     return graph
 
 
+def simplify_graph(graph):
+    # convert path through big caves to multi edges
+    # no big cave loops => at most 1 big cave between small caves
+    scave_to_scave_cnt = Counter()
+    for cave, adj_caves in graph.items():
+        if cave < 0:
+            continue
+        for cave2 in adj_caves:
+            if cave2 >= 0:
+                scave_to_scave_cnt[(cave, cave2)] += 1
+            else:  # cave 2 is an inbetween big cave
+                for cave3 in graph[cave2]:
+                    # assert cave3 >= 0
+                    scave_to_scave_cnt[(cave, cave3)] += 1
+
+    ngraph = defaultdict(list)
+    for (u, v), k in scave_to_scave_cnt.items():
+        ngraph[u].append((v, k))
+
+    return ngraph
+
+
+# this is a DFS (on the graph of states) where the metric is len(path) + int(revisited)
 def traverse(graph, revisit_small_cave=False):
-    heap_cnt = it.count()
-    init_key = START_NODE, frozenset(), False
-    q = [(0, next(heap_cnt), *init_key)]
-    reach_cnt = Counter({init_key: 1})
     path_cnt = 0
+    init_state = (START_NODE, frozenset({START_NODE}), False)
+    reach_cnt = Counter({init_state: 1})
+    q = deque([init_state])
     while q:
-        small_cnt, _, *key = heappop(q)
-        u, visited, cave_revisited = key
-        n_reached = reach_cnt[tuple(key)]
+        state = q.popleft()
+        u, path, revisited = state
+        n_reached = reach_cnt.pop(state)
 
         if u == END_NODE:
             path_cnt += n_reached
             continue
 
-        for v in graph[u]:
-            if v < 0:
-                next_visited = visited
-                next_cave_revisited = cave_revisited
-                next_small_cnt = small_cnt
-            elif v not in visited:
-                next_visited = visited | {v}
-                next_cave_revisited = cave_revisited
-                next_small_cnt = small_cnt + 1
-            elif revisit_small_cave and not cave_revisited:
-                next_visited = visited | {v}
-                next_cave_revisited = True
-                next_small_cnt = small_cnt + 1
+        for v, k in graph[u]:
+            if v not in path:
+                next_state = (v, path | {v}, revisited)
+            elif revisit_small_cave and not revisited:
+                next_state = (v, path, True)
             else:
                 continue
 
-            next_key = v, next_visited, next_cave_revisited
-            if next_key not in reach_cnt:
-                x = next_small_cnt, next(heap_cnt), *next_key
-                heappush(q, x)
-            reach_cnt[next_key] += n_reached
+            if next_state not in reach_cnt:
+                q.append(next_state)
+            reach_cnt[next_state] += k * n_reached
 
     return path_cnt
 
@@ -86,7 +97,9 @@ def traverse(graph, revisit_small_cave=False):
 def main():
     input = open('input').read().strip()
     # input = open('input_test_3').read().strip()
+
     graph = parse_input(input)
+    graph = simplify_graph(graph)
 
     s1 = traverse(graph, revisit_small_cave=False)
     s2 = traverse(graph, revisit_small_cave=True)
