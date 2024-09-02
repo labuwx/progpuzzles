f6b6abdb485931b14db384fa096fd2e3591743db
Szabolcs Sipos <labuwx@balfug.com>
2024-09-02 13:28:29 +0200

    AoC 2022 Day 8: part 2

--- advent_of_code/2022/8/8.hs
+++ advent_of_code/2022/8/8.hs
@@ -1,6 +1,7 @@
 #!/usr/bin/env runhaskell
 
 {-# OPTIONS -Wno-x-partial #-}
+{-# LANGUAGE LambdaCase #-}
 
 import Control.Monad
 import qualified Control.Monad.Trans.State as State
@@ -8,12 +9,18 @@ import Data.Char
 import Data.Functor
 import Data.List
 import Data.List.Split
+import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Text.Printf
 
 
 trim :: String -> String
 trim = dropWhileEnd isSpace . dropWhile isSpace
+
+
+takeWhile1 :: (a -> Bool) -> [a] -> [a]
+takeWhile1 _ [] = []
+takeWhile1 p (x : xs) = if p x then x : takeWhile1 p xs else [x]
 
 
 type UID = Int
@@ -23,24 +30,19 @@ parseForest :: String -> [[Int]]
 parseForest = map (map (\c -> read [c])) . lines . trim
 
 
-addLabel :: [[Int]] -> [[(UID, Int)]]
-addLabel = snd . addLabel' 0
+forest2Grid :: [[Int]] -> (Int, Int, Map.Map (Int, Int) Int)
+forest2Grid fl = (w, h, pGrid Map.empty 1 1 fl)
   where
-    addLabel' :: UID -> [[Int]] -> (Int, [[(UID, Int)]])
-    addLabel' k [] = (k, [])
-    addLabel' k (r : rs) = (k'', (rLabeled : rsLabeled))
-      where
-        (k', rLabeled) = addLabelRow k r
-        (k'', rsLabeled) = addLabel' k' rs
-    addLabelRow :: UID -> [Int] -> (Int, [(UID, Int)])
-    addLabelRow k [] = (k, [])
-    addLabelRow k (x : xs) = (k', (k, x) : xsLabeled)
-      where
-        (k', xsLabeled) = addLabelRow (k + 1) xs
+    h = length fl
+    w = length $ head fl
+    pGrid :: Map.Map (Int, Int) Int -> Int -> Int -> [[Int]] -> Map.Map (Int, Int) Int
+    pGrid m _ _ [] = m
+    pGrid m _ y ([] : rs) = pGrid m 1 (y + 1) rs
+    pGrid m x y ((c : cs) : rs) = pGrid (Map.insert (x, y) c m) (x + 1) y (cs : rs)
 
 
-addLabelM :: [[Int]] -> [[(UID, Int)]]
-addLabelM forest = State.evalState (addLabel' forest) 0
+addLabel :: [[Int]] -> [[(UID, Int)]]
+addLabel forest = State.evalState (addLabel' forest) 0
   where
     freshLabel :: State.State UID UID
     freshLabel = do
@@ -59,40 +61,23 @@ addLabelM forest = State.evalState (addLabel' forest) 0
             )
 
 
-countVisible :: [[Int]] -> Int
-countVisible forest = Set.size visible4
-  where
-    markVisible :: Set.Set UID -> [[(UID, Int)]] -> Set.Set UID
-    markVisible s [] = s
-    markVisible s (r : rs) = markVisible (markVisibleRow s r (-1)) rs
-
-    markVisibleRow :: Set.Set UID -> [(UID, Int)] -> Int -> Set.Set UID
-    markVisibleRow s [] _ = s
-    markVisibleRow s ((label, x) : xs) prev = markVisibleRow (if x > prev then Set.insert label s else s) xs (max prev x)
-
-    lForest = addLabel forest
-    visible0 = Set.empty :: Set.Set UID
-    visible1 = markVisible visible0 lForest
-    visible2 = markVisible visible1 $ map reverse lForest
-    visible3 = markVisible visible2 $ transpose lForest
-    visible4 = markVisible visible3 $ transpose $ reverse lForest
-
-
 type VisState = State.State (Set.Set UID)
 
 
-countVisibleM :: [[Int]] -> Int
-countVisibleM forest = State.evalState countVisibleM' Set.empty
-  where
-    countVisibleM' :: VisState Int
-    countVisibleM' = do
-        let lForest = addLabelM forest
-        markVisible lForest
-        markVisible $ map reverse lForest
-        markVisible $ transpose lForest
-        markVisible $ transpose $ reverse lForest
-        Set.size <$> State.get
+countVisible :: [[Int]] -> Int
+countVisible forest =
+    flip State.evalState Set.empty $
+        ( do
+            let lForest = addLabel forest
 
+            markVisible lForest
+            markVisible $ map reverse lForest
+            markVisible $ transpose lForest
+            markVisible $ transpose $ reverse lForest
+
+            Set.size <$> State.get
+        )
+  where
     markVisible :: [[(UID, Int)]] -> VisState ()
     markVisible = mapM_ (markVisibleRow (-1))
 
@@ -105,13 +90,27 @@ countVisibleM forest = State.evalState countVisibleM' Set.empty
             else return ()
 
 
+scoreForest :: (Int, Int, Map.Map (Int, Int) Int) -> Int
+scoreForest (w, h, fGrid) = maximum [scoreTree x y | x <- [1 .. w], y <- [1 .. h]]
+  where
+    scoreTree x y = product $ map (length . takeWhile1 (< Map.lookup (x, y) fGrid)) nbrl
+      where
+        nbrl =
+            [ [Map.lookup (xx, y) fGrid | xx <- [x + 1 .. w]]
+            , [Map.lookup (xx, y) fGrid | xx <- [x - 1, x - 2 .. 1]]
+            , [Map.lookup (x, yy) fGrid | yy <- [y + 1 .. h]]
+            , [Map.lookup (x, yy) fGrid | yy <- [y - 1, y - 2 .. 1]]
+            ]
+
+
 main = do
     content <- readFile "input"
     -- content <- readFile "input_test"
     let forest = parseForest content
+    let fGrid = forest2Grid forest
 
     let s1 = countVisible forest
-    let s1M = countVisibleM forest
-    let s2 = 0 :: Int
+    let s2 = scoreForest fGrid
 
-    printf "%d %d\n%d\n" s1 s1M s2
+    print s1
+    print s2
