37f047e0aaab01da1979e54f21a7c6a6d8f03f0a
Szabolcs Sipos <labuwx@balfug.com>
2024-09-12 16:06:54 +0200

    AoC 2022 Day 10: part 2

--- advent_of_code/2022/10/10.hs
+++ advent_of_code/2022/10/10.hs
@@ -30,6 +30,18 @@ doOps ops = 1 : doOps' 1 ops
     doOps' x (AddX dx : ops) = let x' = x + dx in ([x, x'] ++ doOps' x' ops)
 
 
+render :: [Int] -> String
+render = tail . render' 0
+  where
+    visible p x = abs (x - p) <= 1
+    render' k (x : xs) = sepChar ++ (currentChar : rest)
+      where
+        xpos = k `mod` 40
+        sepChar = if xpos == 0 then "\n" else ""
+        currentChar = if visible xpos x then '#' else ' '
+        rest = if k < 239 then render' (k + 1) xs else ""
+
+
 main = do
     content <- readFile "input"
     -- content <- readFile "input_test"
@@ -37,7 +49,7 @@ main = do
 
     let res = doOps ops
     let s1 = sum [k * x | (k, x) <- zip [1 ..] res, k `elem` [20, 60, 100, 140, 180, 220]]
-    let s2 = 0
+    let s2 = render res
 
     print s1
-    print s2
+    putStr s2
