1690793ec32027a949e32320f399c65aae5f17ce
Szabolcs Sipos <labuwx@balfug.com>
2025-12-11 01:38:07 +0100

    AoC 2025 Day 10: part 2

--- advent_of_code/2025/10/10.py
+++ advent_of_code/2025/10/10.py
@@ -3,6 +3,9 @@
 
 from collections import deque
 
+import numpy as np
+from scipy.optimize import LinearConstraint, milp
+
 
 def main():
     input = open('input').read()
@@ -16,7 +19,7 @@ def main():
         jolt = tuple(int(x) for x in jolt[1:-1].split(','))
         M.append((ind, btns, jolt))
 
-    s1 = 0
+    s1 = s2 = 0
     for ind, btns, jolt in M:
         r = set()
         q = deque([(0, 0, tuple(False for _ in ind))])
@@ -36,8 +39,18 @@ def main():
                     ind__[b] = not ind__[b]
                 q.append((k + 1, i + j, tuple(ind__)))
 
+        A = np.array(
+            [[int(i in btn) for i in range(len(jolt))] for btn in btns]
+        ).transpose()
+        b = np.array(jolt)
+        c = np.ones(len(btns))
+        res = milp(
+            c=c, constraints=LinearConstraint(A, b, b), integrality=np.ones_like(c)
+        )
+        s2 += int(res.fun)
+
     print(s1)
-    # print(s2)
+    print(s2)
 
 
 main()
